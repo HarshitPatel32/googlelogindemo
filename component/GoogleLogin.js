634a2e1f943c744780509116df3b7dc6161e3da2
Harshit Patel <121165660+HarshitPatel32@users.noreply.github.com>
2024-05-09 22:01:14 +0530

    Merge

--- component/GoogleLogin.js
+++ component/GoogleLogin.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { signIn } from "next-auth/react";
+import Image from "next/image";
 
-const GoogleLogin = ({ data }) => {
-  return <section>Hello</section>;
+const GoogleLogin = () => {
+  const handleSignInWithGoogle = async () => {
+    signIn("google");
+  };
+
+  return (
+    <section className="buttonsection">
+      <div className="google" onClick={handleSignInWithGoogle}>
+        <p className="text">
+          <span className="icon">
+            <Image src="/images/Google.svg" alt="Google" width={24} height={24} />
+          </span>
+          Continue with Google
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default GoogleLogin;
