adf2b572cf0d5b2f1c272685019bcd1ab6292029
HarshitPatel32 <121165660+HarshitPatel32@users.noreply.github.com>
2024-05-15 18:37:00 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -69,10 +69,6 @@ const securityHeaders = [
     key: 'Cross-Origin-Opener-Policy',
     value: 'same-origin-allow-popups',
   },
-  {
-    key: 'Cross-Origin-Embedder-Policy',
-    value: 'require-corp',
-  },
 ];
 
 export default nextConfig;
