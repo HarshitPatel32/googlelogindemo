4fba1d049119d59a48014a509a1527c2973d5d82
HarshitPatel32 <121165660+HarshitPatel32@users.noreply.github.com>
2024-05-15 14:56:22 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <GoogleOAuthProvider clientId={process.env.GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId="738272769394-ar32r2u6pgnl8ejtkrvojh7k6drnfkk8.apps.googleusercontent.com">
       <SessionProvider session={pageProps.session}>
         <Component {...pageProps} />
       </SessionProvider>
