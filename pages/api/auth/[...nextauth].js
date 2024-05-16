7a46ae3fc1355f1d9689f531a5ea10713683fdc4
HarshitPatel32 <121165660+HarshitPatel32@users.noreply.github.com>
2024-05-16 16:44:27 +0530

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,17 +4,6 @@ import CredentialsProvider from "next-auth/providers/credentials";
 const decodeJwtResponse = (token) => {
   var base64Url = token.split(".")[1];
   var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-  // var jsonPayload = decodeURIComponent(
-  //   window
-  //     .atob(base64)
-  //     .split("")
-  //     .map(function (c) {
-  //       return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-  //     })
-  //     .join("")
-  // );
-  // return JSON.parse(jsonPayload);
-
   let buffer = Buffer.from(base64, "base64");
   let decodedPayload = JSON.parse(buffer.toString("utf-8"));
   return decodedPayload;
