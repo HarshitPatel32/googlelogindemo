7c91746dac2af99caf1b0231e33ee487dc89862e
HarshitPatel32 <121165660+HarshitPatel32@users.noreply.github.com>
2024-05-16 15:05:41 +0530

    Update GoogleLog.js

--- component/GoogleLog.js
+++ component/GoogleLog.js
@@ -1,75 +1,50 @@
 import React from "react";
+import axios from "axios";
 import { signIn, useSession, signOut } from "next-auth/react";
-import { GoogleLogin } from "@react-oauth/google";
+import { GoogleLogin, useGoogleLogin } from "@react-oauth/google";
+import Image from "next/image";
 
 const GoogleLog = () => {
   const { data: session } = useSession();
   const user = session?.session?.user;
 
-  // useGoogleOneTapLogin({
-  //   onSuccess: credentialResponse => {
-  //     console.log(credentialResponse);
-  //   },
-  //   onError: () => {
-  //     console.log('Login Failed');
-  //   },
-  // });
+  const handleSignInWithGoogle = useGoogleLogin({
+    onSuccess: async (codeResponse) => {
+      if (codeResponse && codeResponse.access_token) {
+        try {
+          const res = await axios.get(
+            `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${codeResponse.access_token}`,
+            {
+              headers: {
+                Authorization: `Bearer ${codeResponse.access_token}`,
+                Accept: "application/json",
+              },
+            }
+          );
 
-  // const handleSignInWithGoogle = useGoogleLogin({
-  //   onSuccess: async (codeResponse) => {
-  //     console.log(codeResponse);
-  //     if (codeResponse && codeResponse.access_token) {
-  //       try {
-  //         const res = await axios.get(
-  //           `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${codeResponse.access_token}`,
-  //           {
-  //             headers: {
-  //               Authorization: `Bearer ${codeResponse.access_token}`,
-  //               Accept: "application/json",
-  //             },
-  //           }
-  //         );
+          const signInResult = await signIn("credentials", {
+            email: res.data.email,
+            password: "",
+            redirect: false,
+          });
 
-  //         const signInResult = await signIn("credentials", {
-  //           email: res.data.email,
-  //           password: "",
-  //           redirect: false,
-  //         });
-
-  //         if (signInResult?.error) {
-  //           console.error("Sign in failed:", signInResult.error);
-  //         } else {
-  //           console.log("Sign in successful");
-  //         }
-  //       } catch (error) {
-  //         console.error("Error fetching user info:", error);
-  //       }
-  //     }
-  //   },
-  //   onError: (error) => {
-  //     console.error("Login Failed:", error);
-  //   },
-  // });
-
-  const decodeJwtResponse = (token) => {
-    var base64Url = token.split(".")[1];
-    var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-    var jsonPayload = decodeURIComponent(
-      window
-        .atob(base64)
-        .split("")
-        .map(function (c) {
-          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-        })
-        .join("")
-    );
-
-    return JSON.parse(jsonPayload);
-  };
+          if (signInResult?.error) {
+            console.error("Sign in failed:", signInResult.error);
+          } else {
+            console.log("Sign in successful");
+          }
+        } catch (error) {
+          console.error("Error fetching user info:", error);
+        }
+      }
+    },
+    onError: (error) => {
+      console.error("Login Failed:", error);
+    },
+  });
 
   const handleSuccess = (response) => {
-    console.log(response.Credential);
-    console.log(decodeJwtResponse(response.Credential));
+    console.log(response);
   };
 
   const handleError = () => {
@@ -78,16 +53,15 @@ const GoogleLog = () => {
 
   return (
     <section className="buttonsection">
-      <GoogleLogin
-        onSuccess={handleSuccess}
-        onFailure={handleError}
-        useOneTap
-        // shape="pill"
-        // theme="outline"
-        // size="large"
-        // text="continue_with"
-        // width="400"
-      />
+      <div className="google" onClick={handleSignInWithGoogle}>
+        <p className="text">
+          <span className="icon">
+            <Image src="/images/Google.svg" alt="Google" width={24} height={24} />
+          </span>
+          Continue with Google
+        </p>
+      </div>
+      <GoogleLogin onSuccess={handleSuccess} onError={handleError} />
       {user && (
         <div className="logout">
           <label>{user?.email}</label>
